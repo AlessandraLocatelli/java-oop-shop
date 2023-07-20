ba30fa6146bc6dcc6c3daf7985fff497f7a504ba
Ales <alocatelli798@gmail.com>
2023-07-20 17:04:21 +0200

    complete

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,2 +1,35 @@
-package org.lessons.java.shop;public class Main {
+package org.lessons.java.shop;
+import java.util.Scanner;
+
+/* create uno o più prodotti e testate tutte
+le funzionalità della classe (cioè tutti i metodi public)
+*/
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Product product = new Product();
+        System.out.println("Product name: ");
+        String name = sc.nextLine();
+        System.out.println("Product description: ");
+        String description = sc.nextLine();
+        System.out.println("Product price: ");
+        double price = Double.parseDouble(sc.nextLine());
+        System.out.println("Product vat: ");
+        double vat = Double.parseDouble(sc.nextLine());
+
+        product = new Product(name, description, price, vat);
+        System.out.println("Product: "+product);
+        System.out.println("Code: "+product.getProductFullCode(name));
+        System.out.println("Prize with vat: "+product.getPriceWithVat(vat));
+
+
+
+
+        sc.close();
+
+    }
 }
--- src/org/lessons/java/shop/Product.java
+++ src/org/lessons/java/shop/Product.java
@@ -1,2 +1,121 @@
-package org.lessons.java.shop;public class Product {
+package org.lessons.java.shop;
+
+
+
+
+//classe che gestisce i prodotti dello shop.
+/*I prodotti sono caratterizzati da:
+codice (numero intero)
+nome
+descrizione
+prezzo
+iva
+*/
+//usare livelli di accesso, costruttori, get e set, altri metodi in modo che:
+/*
+-alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
+-Il codice prodotto sia accessibile solo in lettura    get
+-Gli altri attributi siano accessibili sia in lettura che in scrittura  set/get
+-Il prodotto esponga sia un metodo per avere il prezzo base
+che uno per avere il prezzo comprensivo di iva
+-Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice-nome
+ il codice deve avere un pad left di zeri per arrivare a 8 caratteri
+ (ad esempio codice 91 diventa 00000091, mentre codice 123445567 resta come è)
+
+ */
+
+import java.util.Random;
+
+public class Product {
+
+    //CAMPI
+
+    private String name;
+    private String description;
+    private double price;
+    private double vat;
+
+    private int code;
+
+
+
+    //COSTRUTTORI
+
+
+    public Product() {
+
+
+    }
+
+    public Product(String name, String description, double price, double vat) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.vat = vat;
+    }
+
+    //GET E SET
+    public int getCode()
+      {return code;}
+
+    public String getName()
+    {return name;}
+    public void setName(String name)
+    {this.name = name;}
+
+    public String getDescription()
+    {return description;}
+    public void setDescription(String description)
+    {this.description = description;}
+
+    public double getPrice()
+    {return price;}
+    public void setPrice(double price)
+    {this.price = price;}
+
+    public double getVat() {
+        return vat;
+    }
+
+    public void setVat(double vat) {
+        this.vat = vat;
+    }
+
+    //METODI
+
+
+
+    public String getProductFullCode(String name)
+    {
+
+        Random random = new Random();
+        int code = random.nextInt(1,9999999);
+
+
+        String fullCode = name+code;
+
+        return fullCode;
+
+    }
+
+    public double getPriceWithVat(double vatValue)
+    {
+
+        double priceWithVat = price + (price * vatValue);
+
+        return  priceWithVat;
+
+    }
+
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", vat=" + vat +
+                '}';
+    }
 }
+
